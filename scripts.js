45f6f667f18719c6e601e8f74882f3ada3ef68b2
tiagopazhs <tiagohebert22@gmail.com>
2022-07-26 16:56:16 -0300

    Chang8

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector("#buttonSalvar");
+
+btn.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    const pedido = document.querySelector("#pedido");
+
+    const value = pedido.value;
+
+    console.log(value);
+
+});
