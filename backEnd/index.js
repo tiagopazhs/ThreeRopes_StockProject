2178c390f550ac992fb3fbfbaa1e60932750758d
tiagopazhs <tiagohebert22@gmail.com>
2022-08-03 08:39:17 -0300

    chang9

---
+++ backEnd/index.js
@@ -0,0 +1,22 @@
+// config inicial
+const express = require('express')
+const app = express()
+
+// forma de ler JSON / middlewares
+app.use(express.urlencoded({
+  extended: true,
+}),
+)
+
+app.use(express.json())
+
+// rota inicial / endpoint
+
+app.get('/', (req, res) => {
+
+  res.json({message: 'Oi Joao!'})
+
+})
+
+// entregar uma porta
+app.listen(3000)
