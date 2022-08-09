e6861f60ba5466fb103996bca079f6d589ea0a43
tiagopazhs <tiagohebert22@gmail.com>
2022-08-09 15:35:35 -0300

    Chang43

--- frontEnd/scripts/scripts.js
+++ frontEnd/scripts/scripts.js
@@ -1,6 +1,18 @@
-const btn = document.querySelector("#buttonSalvar");
+//here
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+//here
 
+const btn = document.querySelector("#buttonSalvar");
 btn.addEventListener("click", function(e) {
+
+//here
+    useEffect(() => {
+        axios.get("http://localhost:3000/person")
+        .then(()=>{console.log("Deu tudo certo")})
+        .catch(()=>{console.log("Deu tudo errado")})
+    },[]) // este colchete serve para definir que a funcao sera executada somente uma vez
+//here
 
     e.preventDefault();
 
